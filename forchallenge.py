f4de918a04d043a958ed70556e851cb6e115bd99
Phouthalang Pygnasak <phou@Phouthalangs-MacBook-Pro.local>
2017-09-09 20:39:18 -0700

    add more comments

--- forchallenge.py
+++ forchallenge.py
@@ -10,20 +10,20 @@ for ip in ipAddress:
     else:
 
         segmentCount = 1 # set segment count to one
-        checkFirstIdx = 0 #
-        message = ''
+        checkFirstIdx = 0 # use for verifying first index does not contain deliminator '.'
+        errorMessage = '' # store error message if invalid
         for ipSegment in ipAddress[ip]: # get character value
 
             if (ipSegment == '.' and checkFirstIdx >= 1): # check first indexl; does it contain an invalid start deliminator
                 segmentCount += 1
             elif (checkFirstIdx == 0 and ipSegment == '.' ): # check first index only for '.'
-                message = '{0} IP address is invalid'.format(ipAddress[ip])  # assign error message
+                errorMessage = '{0} IP address is invalid'.format(ipAddress[ip])  # assign error message
                 break # break loop, IP has an invalid start deliminator
 
             checkFirstIdx += 1
 
-        if (message != ''): # if message is not empty, then print error
-            print(message)
+        if (errorMessage != ''): # if message is not empty, then print error
+            print(errorMessage)
         else:
             print('{0} IP address has {1} segments'.format(ipAddress[ip], segmentCount)) # print number of segments if valid IP address
 
